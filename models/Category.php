2c708660e428e924746bef00fca232467f85788c
SamuelePassiatore <semmisno2@protonmail.com>
2023-02-20 15:14:04 +0100

    added category class with constructor

---
+++ models/Category.php
@@ -0,0 +1,12 @@
+<?php
+class Category
+{
+    //!Properties or istance variables
+    public $name;
+
+    //!Class constructor
+    public function __construct($name)
+    {
+        $this->name = $name;
+    }
+}
