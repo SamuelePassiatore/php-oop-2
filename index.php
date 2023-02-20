4d8776635c49be02b3ff6651fce88fa6fb4b4258
SamuelePassiatore <semmisno2@protonmail.com>
2023-02-20 15:17:56 +0100

    added categories variable to product class

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-require_once __DIR__ . '/models/Product.php';
-
+include_once __DIR__ . '/models/Product.php';
+include_once __DIR__ . '/models/Category.php';
 ?>
 
 
--- models/Product.php
+++ models/Product.php
@@ -1,20 +1,21 @@
 <?php
+require_once __DIR__ . '/Category.php';
 class Product
 {
     //!Properties or istance variables
     public $image;
     public $title;
     public $price;
-    public $category;
+    public $categories;
     public $type;
 
     //!Class constructor
-    public function __construct($image, $title, $price, $category, $type)
+    public function __construct($image, $title, $price, Category $categories, $type)
     {
         $this->image = $image;
         $this->title = $title;
         $this->price = $price;
-        $this->category = $category;
+        $this->categories = $categories;
         $this->type = $type;
     }
 }
