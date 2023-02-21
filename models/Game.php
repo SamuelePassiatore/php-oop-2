c12c5e271ff621eeb75aaa242da07da40b26e315
SamuelePassiatore <semmisno2@protonmail.com>
2023-02-21 17:53:47 +0100

    use shipping in game and product & print two method shipping

--- models/Game.php
+++ models/Game.php
@@ -1,15 +1,20 @@
 <?php
 require_once __DIR__ . '/Product.php';
+require_once __DIR__ . '../../traits/Shipping.php';
+
 
 class Game extends Product
 {
+    use Shipping;
     public $material;
     public $color;
 
-    public function __construct($image, $title, $price, Category $categories, $type, $material, $color)
+    public function __construct($image, $title, $price, Category $categories, $type, $material, $color, $shippingCourier, $shippingCost)
     {
         parent::__construct($image, $title, $price, $categories, $type);
         $this->material = $material;
         $this->color = $color;
+        $this->shippingCourier = $shippingCourier;
+        $this->shippingCost = $shippingCost;
     }
 }
